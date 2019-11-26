fddb718054a8bac272a2be2dd27aa1bfe50cbfff
daikimiura <daikiii5555@gmail.com>
2019-11-26 22:23:54 +0900

    Initial commit

---
+++ spec/jarvis_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Jarvis do
+  it "has a version number" do
+    expect(Jarvis::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
